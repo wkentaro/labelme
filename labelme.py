dfbeb448ac669cce7c64340af7ae84f5d22ce10d
Hussein <scrubbed>
2011-10-15 21:12:05 +0100

    fix copy and delete status after copying a shape

--- labelme.py
+++ labelme.py
@@ -230,6 +230,7 @@ class MainWindow(QMainWindow, WindowMixin):
 
     def copySelectedShape(self):
         self.addLabel(self.canvas.copySelectedShape())
+        #fix copy and delete
         self.shapeSelectionChanged(True)
 
     def highlightLabel(self, item):
