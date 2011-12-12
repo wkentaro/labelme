0b472517c14ab7427a61881ecec13e4e7c412493
Michael Pitidis <mpitid@gmail.com>
2011-12-12 11:29:11 +0000

    Add GPL v3 license and headers

--- lib.py
+++ lib.py
@@ -1,3 +1,21 @@
+#
+# Copyright (C) 2011 Michael Pitidis, Hussein Abdulwahid.
+#
+# This file is part of Labelme.
+#
+# Labelme is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Labelme is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Labelme.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 from math import sqrt
 
