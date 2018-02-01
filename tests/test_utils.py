e2f80ef0bba234883c909cd4fd2900d5935be83a
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-02-02 05:17:36 +0900

    Use pytest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,6 @@
 import json
 import os.path as osp
 
-import nose
 import numpy as np
 
 from labelme import utils
@@ -11,9 +10,9 @@ here = osp.dirname(osp.abspath(__file__))
 
 
 def test_img_b64_to_array():
-    json_file = osp.join(here, '../static/apc2016_obj3.json')
+    json_file = osp.join(here, '../tutorial/apc2016_obj3.json')
     data = json.load(open(json_file))
     img_b64 = data['imageData']
     img = utils.img_b64_to_array(img_b64)
-    nose.tools.assert_equal(img.dtype, np.uint8)
-    nose.tools.assert_equal(img.shape, (907, 1210, 3))
+    assert img.dtype == np.uint8
+    assert img.shape == (907, 1210, 3)
