595ca16714377c1e8267b7a5a8c66d84e1692fe7
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-04-02 03:39:46 +0900

    Add img_b64_to_arr, img_arr_to_b64

--- labelme/utils.py
+++ labelme/utils.py
@@ -57,10 +57,25 @@ def label2rgb(lbl, img=None, n_labels=None, alpha=0.3, thresh_suppress=0):
 
 
 def img_b64_to_array(img_b64):
+    warnings.warn('img_ba64_to_array is deprecated. '
+                  'Please use img_b64_to_arr.')
+    return img_b64_to_arr(img_b64)
+
+
+def img_b64_to_arr(img_b64):
     f = io.BytesIO()
     f.write(base64.b64decode(img_b64))
     img_arr = np.array(PIL.Image.open(f))
     return img_arr
+
+
+def img_arr_to_b64(img_arr):
+    img_pil = PIL.Image.fromarray(img_arr)
+    f = io.BytesIO()
+    img_pil.save(f, format='PNG')
+    img_bin = f.getvalue()
+    img_b64 = base64.encodestring(img_bin)
+    return img_b64
 
 
 def polygons_to_mask(img_shape, polygons):
