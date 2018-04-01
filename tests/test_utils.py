24259a5810c20a98a7b980fa818129a746b2ac20
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-04-02 03:39:46 +0900

    Add img_b64_to_arr, img_arr_to_b64

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,17 +2,27 @@ import json
 import os.path as osp
 
 import numpy as np
+import PIL.Image
 
-from labelme import utils
+import labelme
 
 
 here = osp.dirname(osp.abspath(__file__))
+data_dir = osp.join(here, 'data')
 
 
-def test_img_b64_to_array():
-    json_file = osp.join(here, '../examples/single_image/apc2016_obj3.json')
+def test_img_b64_to_arr():
+    json_file = osp.join(data_dir, 'apc2016_obj3.json')
     data = json.load(open(json_file))
     img_b64 = data['imageData']
-    img = utils.img_b64_to_array(img_b64)
+    img = labelme.utils.img_b64_to_arr(img_b64)
     assert img.dtype == np.uint8
     assert img.shape == (907, 1210, 3)
+
+
+def test_img_arr_to_b64():
+    img_file = osp.join(data_dir, 'apc2016_obj3.jpg')
+    img_arr = np.asarray(PIL.Image.open(img_file))
+    img_b64 = labelme.utils.img_arr_to_b64(img_arr)
+    img_arr2 = labelme.utils.img_b64_to_arr(img_b64)
+    np.testing.assert_allclose(img_arr, img_arr2)
