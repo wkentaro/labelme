c70c395c49d9e77b5acb2d0b55327a8c7856bd1d
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-04-26 02:28:04 +0900

    Add --version option

--- labelme/__init__.py
+++ labelme/__init__.py
@@ -1,9 +1,11 @@
 # flake8: noqa
 
 import logging
+import pkg_resources
 
 
 __appname__ = 'labelme'
+__version__ = pkg_resources.get_distribution(__appname__).version
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__appname__)
