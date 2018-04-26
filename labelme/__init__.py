6b65fa3723a1ac41e068455be6589d032a193400
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-04-26 23:44:52 +0900

    Stop using pkg_resources.get_distribution for version

--- labelme/__init__.py
+++ labelme/__init__.py
@@ -1,15 +1,15 @@
 # flake8: noqa
 
 import logging
-import pkg_resources
 
 
 __appname__ = 'labelme'
-__version__ = pkg_resources.get_distribution(__appname__).version
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__appname__)
 
+
+from labelme._version import __version__
 
 from labelme import testing
 from labelme import utils
