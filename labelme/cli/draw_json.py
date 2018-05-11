e1b3ec3dd5b47a03c99b548a75e26dad0749903d
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-05-11 15:08:45 +0900

    Fix style of code

--- labelme/cli/draw_json.py
+++ labelme/cli/draw_json.py
@@ -22,13 +22,14 @@ def main():
 
     data = json.load(open(json_file))
 
-    if data['imageData'] is not None:
+    if data['imageData']:
         imageData = data['imageData']
     else:
         imagePath = os.path.join(os.path.dirname(json_file), data['imagePath'])
         with open(imagePath, 'rb') as f:
             imageData = f.read()
             imageData = base64.b64encode(imageData).decode('utf-8')
+
     img = utils.img_b64_to_arr(imageData)
 
     label_name_to_value = {'_background_': 0}
