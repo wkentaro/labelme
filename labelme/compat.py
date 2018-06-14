dc1fd35fdeee5bb0a27e2de06951cb0aa35ed39d
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-06-14 18:46:22 +0900

    Fix for PyQt4

--- labelme/compat.py
+++ labelme/compat.py
@@ -1,15 +1,7 @@
 from qtpy import QT_VERSION
-from qtpy import QtCore
 
 
 QT5 = QT_VERSION[0] == '5'
 
 
-if QT5:
-    QPoint = QtCore.QPoint
-else:
-    QPoint = QtCore.QPointF
-
-
 del QT_VERSION
-del QtCore
