de4ecab70d3191685ca69584961bb140f6ea7369
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-07-21 05:57:11 +0900

    Move widgets from app to labelme.widgets

--- labelme/widgets/__init__.py
+++ labelme/widgets/__init__.py
@@ -4,8 +4,12 @@ from .canvas import Canvas
 
 from .color_dialog import ColorDialog
 
+from .escapable_qlist_widget import EscapableQListWidget
+
 from .label_dialog import LabelDialog
 from .label_dialog import LabelQLineEdit
+
+from .label_qlist_widget import LabelQListWidget
 
 from .tool_bar import ToolBar
 
