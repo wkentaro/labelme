6b09fbbb18328b1b298b92eaac6972db939aa792
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-08-04 07:44:57 +0900

    Fix label visualization in draw_json, json_to_dataset

--- labelme/cli/draw_json.py
+++ labelme/cli/draw_json.py
@@ -30,15 +30,17 @@ def main():
         with open(imagePath, 'rb') as f:
             imageData = f.read()
             imageData = base64.b64encode(imageData).decode('utf-8')
-
     img = utils.img_b64_to_arr(imageData)
 
-    lbl, label_name_to_value = utils.shapes_to_label(img.shape, data['shapes'])
-
-    lbl = utils.shapes_to_label(
-        img.shape, shapes=data['shapes'],
-        label_name_to_value=label_name_to_value,
-    )
+    label_name_to_value = {'_background_': 0}
+    for shape in sorted(data['shapes'], key=lambda x: x['label']):
+        label_name = shape['label']
+        if label_name in label_name_to_value:
+            label_value = label_name_to_value[label_name]
+        else:
+            label_value = len(label_name_to_value)
+            label_name_to_value[label_name] = label_value
+    lbl = utils.shapes_to_label(img.shape, data['shapes'], label_name_to_value)
 
     label_names = [None] * (max(label_name_to_value.values()) + 1)
     for name, value in label_name_to_value.items():
