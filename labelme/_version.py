81d92aa852fa7a150f47286021a7928e057dc628
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-11-06 17:25:48 +0000

    3.5.0

--- labelme/_version.py
+++ labelme/_version.py
@@ -1,1 +1,5 @@
-__version__ = '3.4.1'
+# Semantic Versioning 2.0.0: https://semver.org/
+# 1. MAJOR version when you make incompatible API changes;
+# 2. MINOR version when you add functionality in a backwards-compatible manner;
+# 3. PATCH version when you make backwards-compatible bug fixes.
+__version__ = '3.5.0'
