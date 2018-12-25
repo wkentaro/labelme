f82373f5806c8c56e801c20d417bb24fb49e3912
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-12-25 19:44:35 +0900

    Check if osp.dirname is empty or not

--- labelme/app.py
+++ labelme/app.py
@@ -926,7 +926,7 @@ class MainWindow(QtWidgets.QMainWindow, WindowMixin):
             imagePath = osp.relpath(
                 self.imagePath, osp.dirname(filename))
             imageData = self.imageData if self._config['store_data'] else None
-            if not osp.exists(osp.dirname(filename)):
+            if osp.dirname(filename) and not osp.exists(osp.dirname(filename)):
                 os.makedirs(osp.dirname(filename))
             lf.save(
                 filename=filename,
