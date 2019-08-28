854f039fd5b39771b6602594bbbf0acb07346cc3
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-08-28 05:59:30 +0100

    Clip only x1 and y1 for intersectingEdges

--- labelme/widgets/canvas.py
+++ labelme/widgets/canvas.py
@@ -560,12 +560,9 @@ class Canvas(QtWidgets.QWidget):
                   (size.width() - 1, 0),
                   (size.width() - 1, size.height() - 1),
                   (0, size.height() - 1)]
-        # FIXME: below clipping is unnecessary on Linux and Mac,
-        # but required on Windows to avoid error in intersectingEdges
+        # x1, y1 should be in the pixmap, x2, y2 should be out of the pixmap
         x1 = min(max(p1.x(), 0), size.width() - 1)
-        x2 = min(max(p2.x(), 0), size.width() - 1)
         y1 = min(max(p1.y(), 0), size.height() - 1)
-        y2 = min(max(p2.y(), 0), size.height() - 1)
         d, i, (x, y) = min(self.intersectingEdges((x1, y1), (x2, y2), points))
         x3, y3 = points[i]
         x4, y4 = points[(i + 1) % 4]
