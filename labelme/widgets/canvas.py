f2075177d250abaf49347e24a67f7b13c809ef0d
Christian Clauss <cclauss@me.com>
2019-12-27 23:01:20 +0100

    Typo found by codespell

--- labelme/widgets/canvas.py
+++ labelme/widgets/canvas.py
@@ -228,7 +228,7 @@ class Canvas(QtWidgets.QWidget):
                 self.movingShape = True
             return
 
-        # Just hovering over the canvas, 2 posibilities:
+        # Just hovering over the canvas, 2 possibilities:
         # - Highlight shapes
         # - Highlight vertex
         # Update shape/vertex fill and tooltip value accordingly.
