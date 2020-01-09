aa45449197e1d5c7d0510ed22e4caf07f97ea45b
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-01-09 11:44:16 +0000

    Add labelme.utils.img_data_to_arr

--- labelme/utils/__init__.py
+++ labelme/utils/__init__.py
@@ -5,6 +5,7 @@ from ._io import lblsave
 from .image import apply_exif_orientation
 from .image import img_arr_to_b64
 from .image import img_b64_to_arr
+from .image import img_data_to_arr
 from .image import img_data_to_png_data
 
 from .shape import labelme_shapes_to_label
