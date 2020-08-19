6e333ff49864ee2c285f39598aed2ee8f00537ff
leidawt <wxh19970605@126.com>
2020-08-19 12:40:17 +0800

    Coding style fix

--- examples/instance_segmentation/labelme2coco.py
+++ examples/instance_segmentation/labelme2coco.py
@@ -21,8 +21,9 @@ except ImportError:
     print("Please install pycocotools:\n\n    pip install pycocotools\n")
     sys.exit(1)
 
-def rgba2rgb( rgba, background=(255,255,255) ):
-    # convert RGBA to RGB 
+
+def rgba2rgb(rgba, background=(255, 255, 255)):
+    # convert RGBA to RGB
     # https://stackoverflow.com/questions/50331463/convert-rgba-to-rgb-in-python/50332356
 
     row, col, ch = rgba.shape
@@ -32,18 +33,19 @@ def rgba2rgb( rgba, background=(255,255,255) ):
 
     assert ch == 4, 'RGBA image has 4 channels.'
 
-    rgb = np.zeros( (row, col, 3), dtype='float32' )
-    r, g, b, a = rgba[:,:,0], rgba[:,:,1], rgba[:,:,2], rgba[:,:,3]
+    rgb = np.zeros((row, col, 3), dtype='float32')
+    r, g, b, a = rgba[:, :, 0], rgba[:, :, 1], rgba[:, :, 2], rgba[:, :, 3]
 
-    a = np.asarray( a, dtype='float32' ) / 255.0
+    a = np.asarray(a, dtype='float32') / 255.0
 
     R, G, B = background
 
-    rgb[:,:,0] = r * a + (1.0 - a) * R
-    rgb[:,:,1] = g * a + (1.0 - a) * G
-    rgb[:,:,2] = b * a + (1.0 - a) * B
+    rgb[:, :, 0] = r * a + (1.0 - a) * R
+    rgb[:, :, 1] = g * a + (1.0 - a) * G
+    rgb[:, :, 2] = b * a + (1.0 - a) * B
 
-    return np.asarray( rgb, dtype='uint8' )
+    return np.asarray(rgb, dtype='uint8')
+
 
 def main():
     parser = argparse.ArgumentParser(
