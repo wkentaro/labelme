a463e23301d403e7cb3f460701a15eb23dadcdbd
leidawt <wxh19970605@126.com>
2020-08-19 11:51:12 +0800

    fix RGBA error in labelme2coco.py

--- examples/instance_segmentation/labelme2coco.py
+++ examples/instance_segmentation/labelme2coco.py
@@ -21,6 +21,29 @@ except ImportError:
     print("Please install pycocotools:\n\n    pip install pycocotools\n")
     sys.exit(1)
 
+def rgba2rgb( rgba, background=(255,255,255) ):
+    # convert RGBA to RGB 
+    # https://stackoverflow.com/questions/50331463/convert-rgba-to-rgb-in-python/50332356
+
+    row, col, ch = rgba.shape
+
+    if ch == 3:
+        return rgba
+
+    assert ch == 4, 'RGBA image has 4 channels.'
+
+    rgb = np.zeros( (row, col, 3), dtype='float32' )
+    r, g, b, a = rgba[:,:,0], rgba[:,:,1], rgba[:,:,2], rgba[:,:,3]
+
+    a = np.asarray( a, dtype='float32' ) / 255.0
+
+    R, G, B = background
+
+    rgb[:,:,0] = r * a + (1.0 - a) * R
+    rgb[:,:,1] = g * a + (1.0 - a) * G
+    rgb[:,:,2] = b * a + (1.0 - a) * B
+
+    return np.asarray( rgb, dtype='uint8' )
 
 def main():
     parser = argparse.ArgumentParser(
@@ -90,6 +113,7 @@ def main():
         out_img_file = osp.join(args.output_dir, "JPEGImages", base + ".jpg")
 
         img = labelme.utils.img_data_to_arr(label_file.imageData)
+        img = rgba2rgb(img)
         imgviz.io.imsave(out_img_file, img)
         data["images"].append(
             dict(
