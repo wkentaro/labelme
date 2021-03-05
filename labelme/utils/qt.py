3b732f58f772c5308047b80ebb8a0e344292a62c
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-03-05 17:15:45 +0900

    Fix RuntimeWarning in distanceToLine

--- labelme/utils/qt.py
+++ labelme/utils/qt.py
@@ -90,6 +90,8 @@ def distancetoline(point, line):
         return np.linalg.norm(p3 - p1)
     if np.dot((p3 - p2), (p1 - p2)) < 0:
         return np.linalg.norm(p3 - p2)
+    if np.linalg.norm(p2 - p1) == 0:
+        return 0
     return np.linalg.norm(np.cross(p2 - p1, p1 - p3)) / np.linalg.norm(p2 - p1)
 
 
