ba3b3b4290e4e8f61445f82b5a12ad47a95826fc
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-11-10 13:28:34 +0000

    Refactoring

--- labelme/widgets/__init__.py
+++ labelme/widgets/__init__.py
@@ -6,6 +6,8 @@ from .canvas import Canvas
 
 from .color_dialog import ColorDialog
 
+from .file_dialog_preview import FileDialogPreview
+
 from .label_dialog import LabelDialog
 from .label_dialog import LabelQLineEdit
 
@@ -17,5 +19,3 @@ from .tool_bar import ToolBar
 from .unique_label_qlist_widget import UniqueLabelQListWidget
 
 from .zoom_widget import ZoomWidget
-
-from .file_dialog_preview import FileDialogPreview
