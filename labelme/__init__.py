308473ff8327c1e91eab127c926c936d3a3e52cc
Kentaro Wada <www.kentaro.wada@gmail.com>
2023-04-13 23:27:46 +0900

    5.2.0+1

--- labelme/__init__.py
+++ labelme/__init__.py
@@ -12,7 +12,9 @@ __appname__ = "labelme"
 # 1. MAJOR version when you make incompatible API changes;
 # 2. MINOR version when you add functionality in a backwards-compatible manner;
 # 3. PATCH version when you make backwards-compatible bug fixes.
-__version__ = "5.2.0"
+# 1.0.0-alpha < 1.0.0-alpha.1 < 1.0.0-alpha.beta < 1.0.0-beta
+# < 1.0.0-beta.2 < 1.0.0-beta.11 < 1.0.0-rc.1 < 1.0.0. == 1.0.0+1
+__version__ = "5.2.0+1"
 
 QT4 = QT_VERSION[0] == "4"
 QT5 = QT_VERSION[0] == "5"
