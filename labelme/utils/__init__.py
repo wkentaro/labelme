b13e6a49a813b99d24dcaf57bf70101370c32040
Kentaro Wada <www.kentaro.wada@gmail.com>
2023-11-26 19:53:25 +0900

    Implement utils.img_arr_to_data

--- labelme/utils/__init__.py
+++ labelme/utils/__init__.py
@@ -4,6 +4,7 @@ from ._io import lblsave
 
 from .image import apply_exif_orientation
 from .image import img_arr_to_b64
+from .image import img_arr_to_data
 from .image import img_b64_to_arr
 from .image import img_data_to_arr
 from .image import img_data_to_pil
