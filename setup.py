77608797376e5b1e137543c5ed56fa60e336c0be
Kentaro Wada <www.kentaro.wada@gmail.com>
2023-12-02 16:18:36 +0900

    Set onnxruntime!=1.16.0 to avoid providers argument requirements

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_install_requires():
         "matplotlib",
         "natsort>=7.1.0",
         "numpy",
-        "onnxruntime>=1.14.1",
+        "onnxruntime>=1.14.1,!=1.16.0",
         "Pillow>=2.8",
         "PyYAML",
         "qtpy!=1.11.2",
