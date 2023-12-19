9b7e1b72ef6046a5d7264116a29276caccca2b0a
anonymousdouble <112695649+anonymousdouble@users.noreply.github.com>
2023-12-19 14:27:01 +1100

    Update util.py
    refactor with With statement to open file to make code more Pythonic

--- tests/labelme_tests/utils_tests/util.py
+++ tests/labelme_tests/utils_tests/util.py
@@ -10,7 +10,8 @@ data_dir = osp.join(here, "../data")
 
 def get_img_and_data():
     json_file = osp.join(data_dir, "annotated_with_data/apc2016_obj3.json")
-    data = json.load(open(json_file))
+    with open(json_file) as f:
+        data = json.load(f)
     img_b64 = data["imageData"]
     img = image_module.img_b64_to_arr(img_b64)
     return img, data
