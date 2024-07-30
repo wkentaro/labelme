e3263ec96e2ad0b34b2b5977f8280f90dcfc8012
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-07-30 19:18:58 +0900

    Implement "AI Text to Rectangles"

--- labelme/ai/__init__.py
+++ labelme/ai/__init__.py
@@ -2,6 +2,9 @@ import gdown
 
 from .efficient_sam import EfficientSam
 from .segment_anything_model import SegmentAnythingModel
+from .text_to_annotation import get_rectangles_from_texts  # NOQA: F401
+from .text_to_annotation import get_shapes_from_annotations  # NOQA: F401
+from .text_to_annotation import non_maximum_suppression  # NOQA: F401
 
 
 class SegmentAnythingModelVitB(SegmentAnythingModel):
