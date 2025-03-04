0667da095640ba856065362490ad0994cd3ed046
Kentaro Wada <www.kentaro.wada@gmail.com>
2025-03-04 18:51:52 +0900

    5.7.0

--- labelme/__init__.py
+++ labelme/__init__.py
@@ -10,7 +10,7 @@ __appname__ = "labelme"
 # 2. MINOR version when you add functionality in a backwards-compatible manner;
 # 3. PATCH version when you make backwards-compatible bug fixes.
 # e.g., 1.0.0a0, 1.0.0a1, 1.0.0b0, 1.0.0rc0, 1.0.0, 1.0.0.post0
-__version__ = "5.6.1"
+__version__ = "5.7.0"
 
 from labelme.label_file import LabelFile
 from labelme import testing
