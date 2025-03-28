6ba57e4a7467f73a6f388716c477639826186db6
Kentaro Wada <www.kentaro.wada@gmail.com>
2025-03-28 18:11:47 +0900

    Mypy check labelme.widgets.tool_bar

--- labelme/widgets/tool_bar.py
+++ labelme/widgets/tool_bar.py
@@ -7,12 +7,12 @@ class ToolBar(QtWidgets.QToolBar):
         super(ToolBar, self).__init__(title)
         layout = self.layout()
         m = (0, 0, 0, 0)
-        layout.setSpacing(0)
-        layout.setContentsMargins(*m)
+        layout.setSpacing(0)  # type: ignore[union-attr]
+        layout.setContentsMargins(*m)  # type: ignore[union-attr]
         self.setContentsMargins(*m)
-        self.setWindowFlags(self.windowFlags() | QtCore.Qt.FramelessWindowHint)
+        self.setWindowFlags(self.windowFlags() | QtCore.Qt.FramelessWindowHint)  # type: ignore[attr-defined]
 
-    def addAction(self, action):
+    def addAction(self, action):  # type: ignore[override]
         if isinstance(action, QtWidgets.QWidgetAction):
             return super(ToolBar, self).addAction(action)
         btn = QtWidgets.QToolButton()
@@ -21,6 +21,6 @@ class ToolBar(QtWidgets.QToolBar):
         self.addWidget(btn)
 
         # center align
-        for i in range(self.layout().count()):
-            if isinstance(self.layout().itemAt(i).widget(), QtWidgets.QToolButton):
-                self.layout().itemAt(i).setAlignment(QtCore.Qt.AlignCenter)
+        for i in range(self.layout().count()):  # type: ignore[union-attr]
+            if isinstance(self.layout().itemAt(i).widget(), QtWidgets.QToolButton):  # type: ignore[union-attr]
+                self.layout().itemAt(i).setAlignment(QtCore.Qt.AlignCenter)  # type: ignore[attr-defined,union-attr]
