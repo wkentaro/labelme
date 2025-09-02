ad0ee1d64f269833a825c208c780e8ae4c998ef5
Kentaro Wada <www.kentaro.wada@gmail.com>
2025-09-02 23:39:16 +0900

    chore: remove flake8 noqa comments from __init__.py files

--- labelme/utils/__init__.py
+++ labelme/utils/__init__.py
@@ -1,7 +1,4 @@
-# flake8: noqa
-
 from ._io import lblsave
-
 from .image import apply_exif_orientation
 from .image import img_arr_to_b64
 from .image import img_arr_to_data
@@ -11,18 +8,16 @@ from .image import img_data_to_pil
 from .image import img_data_to_png_data
 from .image import img_pil_to_data
 from .image import img_qt_to_arr
-
+from .qt import addActions
+from .qt import distance
+from .qt import distancetoline
+from .qt import fmtShortcut
+from .qt import labelValidator
+from .qt import newAction
+from .qt import newButton
+from .qt import newIcon
 from .shape import labelme_shapes_to_label
 from .shape import masks_to_bboxes
 from .shape import polygons_to_mask
 from .shape import shape_to_mask
 from .shape import shapes_to_label
-
-from .qt import newIcon
-from .qt import newButton
-from .qt import newAction
-from .qt import addActions
-from .qt import labelValidator
-from .qt import distance
-from .qt import distancetoline
-from .qt import fmtShortcut
