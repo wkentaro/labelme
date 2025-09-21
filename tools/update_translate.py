e7f7056bc87b53b4c5dce2595cc7ad98841d9854
Kentaro Wada <www.kentaro.wada@gmail.com>
2025-09-21 18:33:36 +0900

    refactor: type-annotate and move definitions in update_translate.py

--- tools/update_translate.py
+++ tools/update_translate.py
@@ -2,18 +2,17 @@ import pathlib
 import re
 import subprocess
 
-languages = ["zh_CN"]
+here: pathlib.Path = pathlib.Path(__file__).parent
 
 
 def main():
-    labelme_path = pathlib.Path(__file__).parent.parent / "labelme"
-    labelme_translate_path = labelme_path / "translate"
-    assert labelme_translate_path.is_dir(), "translate path not found"
+    labelme_path: pathlib.Path = here / ".." / "labelme"
+    labelme_files: list[pathlib.Path] = list(labelme_path.rglob("*.py"))
 
-    labelme_files = [*labelme_path.rglob("*.py")]
-    for lang in languages:
-        ts_path = labelme_translate_path / f"{lang}.ts"
-        qm_path = labelme_translate_path / f"{lang}.qm"
+    LANGUAGES: list[str] = ["zh_CN"]
+    labelme_translate_path: pathlib.Path = labelme_path / "translate"
+    for lang in LANGUAGES:
+        ts_path: pathlib.Path = labelme_translate_path / f"{lang}.ts"
         subprocess.check_call(
             [
                 "pylupdate5",
@@ -23,12 +22,15 @@ def main():
             ]
         )
         assert ts_path.exists()
+
         # Zero out line numbers to reduce unnecessary diffs in .ts file
-        ts_content = ts_path.read_text()
-        new_ts_content = re.sub(r'line="\d+"', 'line="0"', ts_content)
+        ts_content: str = ts_path.read_text()
+        new_ts_content: str = re.sub(r'line="\d+"', 'line="0"', ts_content)
         assert ts_content.strip() != new_ts_content.strip()
         ts_path.write_text(new_ts_content)
-        subprocess.check_call(["lrelease", str(ts_path), "-qm", str(qm_path)])
+
+        qm_path: pathlib.Path = labelme_translate_path / f"{lang}.qm"
+        subprocess.check_call(["lrelease", ts_path, "-qm", qm_path])
         assert qm_path.exists()
 
 
