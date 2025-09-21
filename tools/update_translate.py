c499ea88823a6d853167456b1c499e3cc87a09d1
Kentaro Wada <www.kentaro.wada@gmail.com>
2025-09-21 18:50:32 +0900

    chore: add fancy logging to update_translate.py

--- tools/update_translate.py
+++ tools/update_translate.py
@@ -1,11 +1,25 @@
 import pathlib
 import re
 import subprocess
+import sys
+
+from loguru import logger
 
 here: pathlib.Path = pathlib.Path(__file__).parent
 
 
 def main():
+    logger.remove(0)
+    logger.level("INFO", color="<dim><white>")
+    logger.add(
+        sys.stderr,
+        level="INFO",
+        colorize=True,
+        format="<level>{message}</level>",
+        backtrace=False,
+        diagnose=False,
+    )
+
     labelme_path: pathlib.Path = here / ".." / "labelme"
     labelme_files: list[pathlib.Path] = list(labelme_path.rglob("*.py"))
 
@@ -28,10 +42,16 @@ def main():
         new_ts_content: str = re.sub(r'line="\d+"', 'line="0"', ts_content)
         assert ts_content.strip() != new_ts_content.strip()
         ts_path.write_text(new_ts_content)
+        logger.info("Updated .ts file: {}", ts_path)
 
         qm_path: pathlib.Path = labelme_translate_path / f"{lang}.qm"
-        subprocess.check_call(["lrelease", ts_path, "-qm", qm_path])
+        subprocess.check_call(
+            ["lrelease", ts_path, "-qm", qm_path],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
         assert qm_path.exists()
+        logger.info("Updated .qm file: {}", qm_path)
 
 
 if __name__ == "__main__":
