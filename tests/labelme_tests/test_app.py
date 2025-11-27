fff2d66c2e3824ecf0bfd87e6da73ac190ad4eda
Kentaro Wada <www.kentaro.wada@gmail.com>
2025-11-27 23:16:26 +0900

    test: add test_image_navigation_while_selecting_shape

--- tests/labelme_tests/test_app.py
+++ tests/labelme_tests/test_app.py
@@ -173,3 +173,31 @@ def test_MainWindow_annotate_jpg(
     win.saveFile()
 
     labelme.testing.assert_labelfile_sanity(out_file)
+
+
+@pytest.mark.gui
+def test_image_navigation_while_selecting_shape(
+    qtbot: QtBot, data_path: pathlib.Path
+) -> None:
+    win: labelme.app.MainWindow = labelme.app.MainWindow(
+        filename=str(data_path / "annotated")
+    )
+    qtbot.addWidget(win)
+    _show_window_and_wait_for_imagedata(qtbot=qtbot, win=win)
+
+    # Incident: https://github.com/wkentaro/labelme/pull/1716 {{
+    point = QPoint(250, 200)
+    qtbot.mouseMove(win.canvas, pos=point)
+    qtbot.mouseClick(win.canvas, Qt.LeftButton, pos=point)
+    qtbot.wait(100)
+
+    qtbot.mouseClick(win.fileListWidget, Qt.LeftButton)
+    qtbot.wait(100)
+
+    qtbot.keyClick(win.fileListWidget, Qt.Key_Down)
+    qtbot.wait(100)
+    qtbot.keyClick(win.canvas, Qt.Key_Down)
+    qtbot.wait(100)
+    # }}
+
+    win.close()
