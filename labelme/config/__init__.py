498cfec5a98f16ad0336fd9225a244ae473c4042
Kentaro Wada <www.kentaro.wada@gmail.com>
2026-01-22 13:25:41 +0900

    refactor: remove unused functions from config module

--- labelme/config/__init__.py
+++ labelme/config/__init__.py
@@ -22,25 +22,6 @@ def update_dict(target_dict, new_dict, validate_item=None):
             target_dict[key] = value
 
 
-# -----------------------------------------------------------------------------
-
-
-def _get_default_config_and_create_labelmerc():
-    config_file = osp.join(here, "default_config.yaml")
-    with open(config_file) as f:
-        config = yaml.safe_load(f)
-
-    # save default config to ~/.labelmerc
-    user_config_file = osp.join(osp.expanduser("~"), ".labelmerc")
-    if not osp.exists(user_config_file):
-        try:
-            shutil.copy(config_file, user_config_file)
-        except Exception:
-            logger.warning(f"Failed to save config: {user_config_file}")
-
-    return config
-
-
 def validate_config_item(key, value):
     if key == "validate_label" and value not in [None, "exact"]:
         raise ValueError(f"Unexpected value for config key 'validate_label': {value}")
@@ -72,27 +53,6 @@ def _migrate_config_from_file(config_from_yaml: dict) -> None:
             model_name_new,
         )
         config_from_yaml["ai"]["default"] = model_name_new
-
-
-def get_config(config_file_or_yaml=None, config_from_args=None):
-    # 1. default config
-    config = _get_default_config_and_create_labelmerc()
-
-    # 2. specified as file or yaml
-    if config_file_or_yaml is not None:
-        config_from_yaml = yaml.safe_load(config_file_or_yaml)
-        if not isinstance(config_from_yaml, dict):
-            with open(config_from_yaml) as f:
-                logger.info(f"Loading config file from: {config_from_yaml}")
-                config_from_yaml = yaml.safe_load(f)
-        _migrate_config_from_file(config_from_yaml=config_from_yaml)
-        update_dict(config, config_from_yaml, validate_item=validate_config_item)
-
-    # 3. command line argument or specified config file
-    if config_from_args is not None:
-        update_dict(config, config_from_args, validate_item=validate_config_item)
-
-    return config
 
 
 def get_user_config_file(create_if_missing: bool = True) -> str:
