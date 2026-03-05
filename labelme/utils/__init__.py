eefc977892308c9618042263a2c4b3f8b637855f
Kentaro Wada <www.kentaro.wada@gmail.com>
2026-03-05 21:15:27 +0900

    refactor!: remove deprecated code
    - Remove deprecated polygons_to_mask() and labelme_shapes_to_label() from labelme.utils
    - Delete legacy labelme_on_docker CLI entry point and labelme/cli/on_docker.py

--- labelme/utils/__init__.py
+++ labelme/utils/__init__.py
@@ -16,8 +16,6 @@ from .qt import labelValidator
 from .qt import newAction
 from .qt import newButton
 from .qt import newIcon
-from .shape import labelme_shapes_to_label
 from .shape import masks_to_bboxes
-from .shape import polygons_to_mask
 from .shape import shape_to_mask
 from .shape import shapes_to_label
