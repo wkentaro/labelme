20f53d897d84ae9884c2f7032b68c423bb5b1e3e
Kentaro Wada <www.kentaro.wada@gmail.com>
2026-03-14 18:57:10 +0900

    refactor: extract _setup_canvas and group canvas widgets into _CanvasWidgets NamedTuple

--- tests/e2e/annotation_test.py
+++ tests/e2e/annotation_test.py
@@ -33,7 +33,7 @@ def test_MainWindow_annotate_jpg(
     show_window_and_wait_for_imagedata(qtbot=qtbot, win=win)
 
     label: str = "whole"
-    canvas_size: QSize = win.canvas.size()
+    canvas_size: QSize = win._canvas_widgets.canvas.size()
     points: list[tuple[float, float]] = [
         (canvas_size.width() * 0.25, canvas_size.height() * 0.25),
         (canvas_size.width() * 0.75, canvas_size.height() * 0.25),
@@ -44,9 +44,13 @@ def test_MainWindow_annotate_jpg(
     qtbot.wait(50)
 
     def click(xy: tuple[float, float]) -> None:
-        qtbot.mouseMove(win.canvas, pos=QPoint(int(xy[0]), int(xy[1])))
+        qtbot.mouseMove(win._canvas_widgets.canvas, pos=QPoint(int(xy[0]), int(xy[1])))
         qtbot.wait(50)
-        qtbot.mousePress(win.canvas, Qt.LeftButton, pos=QPoint(int(xy[0]), int(xy[1])))
+        qtbot.mousePress(
+            win._canvas_widgets.canvas,
+            Qt.LeftButton,
+            pos=QPoint(int(xy[0]), int(xy[1])),
+        )
         qtbot.wait(50)
 
     for xy in points:
@@ -62,13 +66,13 @@ def test_MainWindow_annotate_jpg(
 
     click(xy=points[0])
 
-    assert len(win.canvas.shapes) == 1
-    assert len(win.canvas.shapes[0].points) == 4
-    assert win.canvas.shapes[0].label == "whole"
-    assert win.canvas.shapes[0].shape_type == "polygon"
-    assert win.canvas.shapes[0].group_id is None
-    assert win.canvas.shapes[0].mask is None
-    assert win.canvas.shapes[0].flags == {}
+    assert len(win._canvas_widgets.canvas.shapes) == 1
+    assert len(win._canvas_widgets.canvas.shapes[0].points) == 4
+    assert win._canvas_widgets.canvas.shapes[0].label == "whole"
+    assert win._canvas_widgets.canvas.shapes[0].shape_type == "polygon"
+    assert win._canvas_widgets.canvas.shapes[0].group_id is None
+    assert win._canvas_widgets.canvas.shapes[0].mask is None
+    assert win._canvas_widgets.canvas.shapes[0].flags == {}
 
     win.saveFile()
 
